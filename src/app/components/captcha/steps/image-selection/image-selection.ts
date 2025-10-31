5a870585fa6bfc53b3aebbbd0da61719a89969f5
ybellaho <yanis.bellahouel76@gmail.com>
2025-10-31 14:23:12 +0100

    structure

---
+++ src/app/components/captcha/steps/image-selection/image-selection.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-image-selection',
+  imports: [],
+  templateUrl: './image-selection.html',
+  styleUrl: './image-selection.scss',
+})
+export class ImageSelection {
+
+}
---
+++ src/app/components/captcha/steps/math-problem/math-problem.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-math-problem',
+  imports: [],
+  templateUrl: './math-problem.html',
+  styleUrl: './math-problem.scss',
+})
+export class MathProblem {
+
+}
